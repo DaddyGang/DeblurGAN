a8d8e40081abe06035e731b53dcbb052c87dad21
taldehyde <taldehyde@gmail.com>
2018-03-14 23:49:17 -0400

    Use the center aperture as the ground truth

--- defocus_blur/refocusTest.py
+++ defocus_blur/refocusTest.py
@@ -4,7 +4,7 @@ from scipy.misc import imsave
 import time
 
 LF = np.load('LF_0001.npy')/255
-imsave('real.png', LF[0,0,:,:,:])
+imsave('real.png', LF[4,4,:,:,:])
 start = time.perf_counter()
 imout = refocusAlpha(LF, 1.5)
 end = time.perf_counter()

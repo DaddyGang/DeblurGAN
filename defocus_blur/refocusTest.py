362f3de1b20bb8f02c30b07d69f16f55c0b4e68a
taldehyde <taldehyde@gmail.com>
2018-03-14 23:39:36 -0400

    Implement naive refocus in python3

---
+++ defocus_blur/refocusTest.py
@@ -0,0 +1,12 @@
+import numpy as np
+from refocus import refocusAlpha
+from scipy.misc import imsave
+import time
+
+LF = np.load('LF_0001.npy')/255
+imsave('real.png', LF[0,0,:,:,:])
+start = time.perf_counter()
+imout = refocusAlpha(LF, 1.5)
+end = time.perf_counter()
+print(end-start)
+imsave('out.png', imout)
